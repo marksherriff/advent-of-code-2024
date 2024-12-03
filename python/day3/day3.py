3c2f69e74e028efc9c78f18e0479a11d01e53254
Mark Sherriff <sherriff@virginia.edu>
2024-12-03 18:34:46 -0500

    3-2 complete

--- python/day3/day3.py
+++ python/day3/day3.py
@@ -1,6 +1,6 @@
 import re
 
-TESTING = True
+TESTING = False
 SUM_TOTAL = 0
 DO_DONT = True
 
@@ -10,13 +10,27 @@ if TESTING:
 else:
     f = open("input/input.txt", "r")
 
+## The instructions never specified if we should treat a new line as starting as active or not.
+## So this is a failure in the requirements. :-)
+active = True
 for line in f:
-    results = re.findall("mul[(][0-9]+[,][0-9]+[)]", line)
+    results = re.findall("mul[(][0-9]+[,][0-9]+[)]|don't[(][)]|do[(][)]", line)
     print(results)
+   # active = True
     for result in results:
-        num = result.split(",")
-        num_1 = num[0][4:]
-        num_2 = num[1][:len(num[1])-1]
-        SUM_TOTAL += int(num_1) * int(num_2)
+        print(result)
+        if result == "don't()":
+            print("inactive")
+            active = False
+        elif result == "do()":
+            print("active")
+            active = True
+        elif active:
+            print("math time!")
+            num = result.split(",")
+            num_1 = num[0][4:]
+            num_2 = num[1][:len(num[1])-1]
+            print(num_1, num_2)
+            SUM_TOTAL += int(num_1) * int(num_2)
 
 print(SUM_TOTAL)
